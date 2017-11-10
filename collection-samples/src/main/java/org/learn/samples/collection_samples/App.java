758766e11393507f2a4ae0fae93410d26a0d77b2
visuvit <visuvit@gmail.com>
2017-11-10 17:28:34 +0530

    Initial folder structure

---
+++ collection-samples/src/main/java/org/learn/samples/collection_samples/App.java
@@ -0,0 +1,13 @@
+package org.learn.samples.collection_samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
